b1c6158f127327e3a221618c2b14296f42425b65
Mahekaru <Mahekaru@gmail.com>
2020-03-06 13:45:13 -0500

    changing Text to text and instead of e I'm using event. Typos are killer.

--- js/kanban.js
+++ js/kanban.js
@@ -52,8 +52,8 @@ const create_item = () => {
 document.querySelectorAll('.drop').forEach(element => {
   element.addEventListener('drop',event=>{
     event.preventDefault();
-    const id = event.dataTransfer.getData('Text');
-    e.target.appendChild(document.getElementById(id));
+    const id = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(id));
   });
 
   element.addEventListener('drageover',event=>{
